555e8057c4fec3977aa3295630695d067901ad32
Sebastian ťuczak <fester3000@gmail.com>
2014-06-11 22:22:38 +0200

    Publikacja zasobow

--- Andro/Andro/src/mobi/andromote/andro/androscript/ScriptRepository.java
+++ Andro/Andro/src/mobi/andromote/andro/androscript/ScriptRepository.java
@@ -5,7 +5,7 @@ import java.util.List;
 import mobi.andromote.andro.androscript.datatypes.Script;
 
 public interface ScriptRepository {
-	public void save(Script script);
 	public List<Script> getScriptList();
 	public Script getScript(int id);
+	public void save(Script script);
 }
--- Andro/Andro/src/mobi/andromote/andro/runtimeEnv/RuntimeEnvironmentFacade.java
+++ Andro/Andro/src/mobi/andromote/andro/runtimeEnv/RuntimeEnvironmentFacade.java
@@ -1,12 +1,16 @@
 package mobi.andromote.andro.runtimeEnv;
 
+import org.apache.log4j.Logger;
+
 import mobi.andromote.andro.androscript.datatypes.Script;
 import mobi.andromote.andro.runtimeEnv.controllers.PhoneController;
 import mobi.andromote.andro.runtimeEnv.controllers.RideController;
+import mobi.andromote.andro.webservice.Authenticator;
 
 public enum RuntimeEnvironmentFacade {
 	INSTANCE;
 	
+	private final Logger log = Logger.getLogger(RuntimeEnvironmentFacade.class);
 	CapabilitiesAnalyzer capabilitiesAnalyzer = CapabilitiesAnalyzer.INSTANCE;
 	PhoneController phoneController;
 	RideController rideController;
